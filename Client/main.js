b466f672f63f934192732be026fb6f2b44940d1d
Akachi <86838625+akachi1409@users.noreply.github.com>
2023-02-13 07:50:56 -0500

    remove unused

--- Client/main.js
+++ Client/main.js
@@ -5,10 +5,8 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 const Hypercore = require("hypercore");
 const Hyperswarm = require('hyperswarm')
-const Hyperbee = require("hyperbee");
 const b4a = require('b4a')
 const goodbye = require('graceful-goodbye')
-const { v4: uuidv4 } = require("uuid");
 
 const conns = []
 
@@ -17,12 +15,6 @@ goodbye(() => swarm.destroy())
 const createHyperdrive = async () => {
   const store = new Hypercore("./store", process.env.KEY);
   store.on("ready", async () => {
-    // const db = new Hyperbee(store, {
-    //   keyEncoding: "utf-8",
-    //   valueEncoding: "binary",
-    // });
-    // await db.ready();
-    // global.db = db;
     const foundPeers = store.findingPeers();
     const topic = b4a.from(process.env.TOPIC, 'hex')
     const swarm = new Hyperswarm()
@@ -42,29 +34,17 @@ const createHyperdrive = async () => {
       console.log('*Got Connection:', name, '*')
       conns.push(conn)
       conn.once('close', () => conns.splice(conns.indexOf(conn), 1))
-      // conn.on('data', (data) => {
-      //   window.webContents.send(
-      //     'message:received',
-      //     name,
-      //     b4a.toString(data, 'utf-8')
-      //   )
-      // })
     })
     swarm.flush().then(()=> foundPeers());
     await store.update();
     
     ipcMain.on('message:send', async (event, message) => {
       console.log("message", message);
-      // const uid = uuidv4();
-      // await db.put(uid, JSON.stringify(message));
       for (const conn of conns) {
         conn.write("data:" +message)
       }
     })
-  
-    // ipcMain.on('message:send', (event, message) =>
-    //   window.webContents.send('update-counter', message)
-    // )
+
     window.loadFile(path.join(__dirname) + '/pages/index.html')
     console.log("store's length is ", store.length);
     for await(const block of store.createReadStream({start: 0, live:true})){
@@ -76,11 +56,8 @@ const createHyperdrive = async () => {
       console.log('block', block.toString());
     }
   })
-  // global.drive = drive;
 }
 
 app.whenReady().then(async () => {
-  // await discovery.flushed()
-  // mainWindow()
   createHyperdrive();
 })
